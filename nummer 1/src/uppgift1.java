87d616fa1125a3e2a23ebe48a71eae4c894dc560
oggep <oskper26@gmail.com>
2019-09-16 16:33:51 +0200

    2 första frågorna

--- nummer 1/src/uppgift1.java
+++ nummer 1/src/uppgift1.java
@@ -4,6 +4,13 @@ public class uppgift1 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		System.out.println("Vad heter du?");
+		String str = input.nextLine();
+		
+		System.out.println("Hur gammal är du?");
+		String str2 = input.nextLine();
+		
+		System.out.println(str2);
 		
 		
 		
