c7f2f768a57f15a8508b56d60c58255bb2422762
oggep <oskper26@gmail.com>
2019-09-20 09:37:29 +0200

    Update uppgift2.java

--- nummer 2/src/uppgift2.java
+++ nummer 2/src/uppgift2.java
@@ -5,18 +5,25 @@ public class uppgift2 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		int x = 0;
-		int largest = -1000;
-		int smallest = 1000;
+		int largest = -1000000;
+		int smallest = 1000000;
 		int sum = 0;		
 		
 		for (int i = 0; i < 10; i++) {
 			x = input.nextInt();
 			sum = sum + x;
+			if(x<smallest) {
+				smallest=x;
+			}
+			if(x>largest) {
+				largest=x;
+			}
 			
 		}
 		
 		
 		System.out.println("Medelvärde: " + (float)sum / 10);
-
+		System.out.println("Minsta: " + smallest);
+		System.out.println("Största: " + largest);
 	}
 }
