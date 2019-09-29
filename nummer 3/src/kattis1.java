f85eb199ee9defcbb91140986e3ab236377f52da
oggep <oskper26@gmail.com>
2019-09-29 21:33:02 +0200

    testar igen

--- nummer 3/src/kattis1.java
+++ nummer 3/src/kattis1.java
@@ -6,10 +6,10 @@ public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
 	
 	System.out.println("skriv 2 heltal mellan 1000 och -1000");
-	int r1 = 0;
-	int s = 0;
-	r1 = input.nextInt(); 
-	s = input.nextInt();
+
+	
+	int r1 = input.nextInt(); 
+	int s = input.nextInt();
 	int r2 = 2*s - r1;
 	System.out.println(r2);
 }
