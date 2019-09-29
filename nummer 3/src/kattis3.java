b83edc3b95216d04b947bc4cc2871b813547d97e
oggep <oskper26@gmail.com>
2019-09-29 22:33:30 +0200

    klar

---
+++ nummer 3/src/kattis3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class kattis3 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		int x = input.nextInt() + 1; 
+		for (int i = 1 ; i < x  ; i++){
+			System.out.println(i + "abracadabra");
+        }
+	}
+
+}
