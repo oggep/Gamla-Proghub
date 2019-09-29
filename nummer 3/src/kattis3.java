decdb89cb6a0d4733c3a3c994f31a391fbfe1893
oggep <oskper26@gmail.com>
2019-09-29 22:43:15 +0200

    såja alla rätt på kattis

--- nummer 3/src/kattis3.java
+++ nummer 3/src/kattis3.java
@@ -4,9 +4,10 @@ public class kattis3 {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-		int x = input.nextInt() + 1; 
-		for (int i = 1 ; i < x  ; i++){
-			System.out.println(i + "abracadabra");
+		int x = input.nextInt() + 1 ; 
+		
+		for (int i = 1 ; i < x; i++){
+			System.out.println(i + "\t" + "abracadabra");
         }
 	}
 
