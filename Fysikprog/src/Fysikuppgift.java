8c7f19f5d7a14fb143e067464022a9b11f78cdad
oggep <54796870+oggep@users.noreply.github.com>
2019-11-21 09:58:04 +0100

    1

--- Fysikprog/src/Fysikuppgift.java
+++ Fysikprog/src/Fysikuppgift.java
@@ -1,9 +1,12 @@
-
 public class Fysikuppgift {
 
+	
+	static double G = 9.82;
+	static double R = 8.31446;
+	static double p_0 = 1000;
+	static double C = 299792458;
+	static double g_swe = 9.82;
+	
 	public static void main(String[] args) {
-		
-
 	}
-
 }
