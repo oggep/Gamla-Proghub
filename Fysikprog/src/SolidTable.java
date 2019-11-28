a1143619733963af971e85d89da53d6441b85a77
oggep <54796870+oggep@users.noreply.github.com>
2019-11-28 10:50:15 +0100

    solid

--- Fysikprog/src/SolidTable.java
+++ Fysikprog/src/SolidTable.java
@@ -3,7 +3,10 @@
 public enum SolidTable {
 
 	ICE(0.92, 2.2, 0, 334),
-	IRON(7.87, 0.45, 1538, 276)
+	IRON(7.87, 0.45, 1538, 276),
+	ALUMINUIM(2.70, 0.9, 660, 397),
+	GOLD(19.3, 0.13, 1064, 63),
+	COPPER(8.96, 0.39, 1085, 206);
 	;
 
 	double density;
