0c9e72f798011a1bdefba684e67c375dc40e674c
oggep <54796870+oggep@users.noreply.github.com>
2019-12-05 09:30:12 +0100

    array

---
+++ Hemuppgifter/src/arrays/Torsdagsarray.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class Torsdagsarray {
+	public static void main(String[] args) {
+	int years = 0;
+	String[] names = {"Tomas", "Tomas", "Tomas", "Are", "Tomas", "Are", "Tomas", "Are", "Tomas", "Niklas"};
+	for(String name: names) {
+	if(name.equals("Are")) {
+		years++;
+	}	
+		
+	}
+	System.out.println(years);
+	}
+}
