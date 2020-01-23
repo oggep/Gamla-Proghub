6cdb50a504f77cf4ce9cb050286ff11d7d1d57e9
oggep <54796870+oggep@users.noreply.github.com>
2020-01-23 10:30:28 +0100

    update

--- Sortering/Bubblesort.java
+++ Sortering/Bubblesort.java
@@ -7,7 +7,16 @@ public class Bubblesort {
 	}
 	void bubblesort(int arr[]) {
 		int n = arr.length;
-		for
+		for(int i = 0; i < n-1; i++) {
+			for (int j = 0; j < n-i-1; j++) {
+				if (arr[j] > arr[j+1]) {
+					
+					
+				}
+			}
+			
+			
+		}
 		
 	}
 
