3f0d1b389238f9cf00aa4039beeb972f1b4757df
oggep <54796870+oggep@users.noreply.github.com>
2020-02-05 22:43:02 +0100

    fixat

--- Kattis2/bijele.java
+++ Kattis2/bijele.java
@@ -3,15 +3,16 @@ public class bijele{
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		//Först gör jag mina arrayer för att ha värdena som behövdes.
 		int[] staff = {1, 1, 2, 2, 2, 8};
 		int[] number = new int[6];
 		
 		
-			
+			//Sen gör jag en loop för man skriva in rätt antal med tal och vilka som man ska räkna med
 			for (int i = 0; i < number.length; i++) {    
 				number[i] = input.nextInt();
-				
-			System.out.println(staff[i] - number[i] + " ");
+			//Med denna räknar jag ut resultat på varje rad.	
+			System.out.println(staff[i] - number[i]);
 			}
 		}
 	}
---
+++ Kattis2/ihavebeeneverywhereman.java
@@ -0,0 +1,9 @@
+
+public class ihavebeeneverywhereman {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
