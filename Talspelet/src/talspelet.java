e86283f9bfc34c0615757696a2bb5de12b814972
oggep <54796870+oggep@users.noreply.github.com>
2020-03-20 09:05:28 +0100

    talspel

--- Talspelet/src/talspelet.java
+++ Talspelet/src/talspelet.java
@@ -1,28 +1,31 @@
-import java.util.
+import java.util.Random;
 import java.util.Scanner;
 public class talspelet {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Vilken svårighetsgrad? Easy, Normal, Hard");
-        String Difficulty = input.nextLine();
-        if(Difficulty == "Easy"){
-            Easy();
+        System.out.println("Vilken svårighetsgrad?  Easy, Normal, Hard");
+        System.out.println("1. Easy");
+        System.out.println("2. Normal");
+        System.out.println("1. Hard");
+        int Difficulty = input.nextInt();
+        if(Difficulty == 1){
+            easy();
         }
-        if(Difficulty == "Normal"){
-            Normal();
+        if(Difficulty == 2){
+            normal();
         }
-        if(Difficulty == "Hard"){0
-            Hard();
+        if(Difficulty == 3){
+            hard();
         }
     }
 
-    private static void Hard() {
+    private static void easy() {
 
     }
 
-    private static void Normal() {
+    private static void normal() {
     }
 
-    private static void Easy() {
+    private static void hard() {
     }
 }
