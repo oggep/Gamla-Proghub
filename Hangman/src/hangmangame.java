42b932b55e403815c6ac92e3dce356590ae31e91
oggep <54796870+oggep@users.noreply.github.com>
2020-05-14 08:51:03 +0200

    Start

---
+++ Hangman/src/hangmangame.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Random;
+public class hangmangame {
+    public static void main(String[] args) {
+        startMenu();
+    }
+    public static void startMenu{
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Hangman");
+
+
+    }
+
+}
