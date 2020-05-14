d0ad5dbf3ed6d389f28253501885b0ff025777ab
oggep <54796870+oggep@users.noreply.github.com>
2020-05-14 16:39:07 +0200

    commit 2

--- Hangman/src/hangmangame.java
+++ Hangman/src/hangmangame.java
@@ -1,12 +1,32 @@
 import java.util.Scanner;
 import java.util.Random;
 public class hangmangame {
-    public static void main(String[] args) {
+    String[] shortwords = {"car", "foot", "rain", "sand", "cow", "shoe","dirt"};
+    String[] mediumwords = {"forest", "bottle", "teeth", "bored", "forced", "knife"};
+    String[] longwords = {"skydiving", "Communicate", "friendzone", "vandalize", "shockproof", "advanced"};
+    public static void main(String[] args){
         startMenu();
     }
-    public static void startMenu{
+    public static void startMenu(){
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Hangman");
+        System.out.println("What length of words do you want?");
+        System.out.println("1. Short");
+        System.out.println("2. Medium");
+        System.out.println("3. Long");
+        int menu = input.nextInt();
+        switch (menu){
+            case 1:
+                System.out.println("Redirecting you to short words...");
+                break;
+            case 2:
+                System.out.println("Redirecting you to medium words...");
+                break;
+            case 3:
+                System.out.println("Redirecting you to long words...");
+                break;
+
+        }
 
 
     }
