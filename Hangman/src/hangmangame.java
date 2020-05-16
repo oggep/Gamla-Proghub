f5b05a5f9211d21a4e5b505b3c84bda1f8f55c75
oggep <54796870+oggep@users.noreply.github.com>
2020-05-16 21:29:47 +0200

    Lite extra kod

--- Hangman/src/hangmangame.java
+++ Hangman/src/hangmangame.java
@@ -1,23 +1,27 @@
 import java.util.Scanner;
-import java.util.Random;
+
 public class hangmangame {
-    String[] shortwords = {"car", "foot", "rain", "sand", "cow", "shoe","dirt"};
-    String[] mediumwords = {"forest", "bottle", "teeth", "bored", "forced", "knife"};
-    String[] longwords = {"skydiving", "Communicate", "friendzone", "vandalize", "shockproof", "advanced"};
+    private static int menu;
+    private static String[] shortwords = {"car", "foot", "rain", "sand", "cow", "shoe","dirt"};
+    private static String[] mediumwords = {"forest", "bottle", "teeth", "bored", "forced", "knife"};
+    private static String[] longwords = {"skydiving", "Communicate", "friendzone", "vandalize", "shockproof", "advanced"};
+    int menu = 0;
+    private static String word ="";
     public static void main(String[] args){
         startMenu();
     }
-    public static void startMenu(){
+    public static String startMenu(){
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Hangman");
         System.out.println("What length of words do you want?");
         System.out.println("1. Short");
         System.out.println("2. Medium");
         System.out.println("3. Long");
-        int menu = input.nextInt();
+        menu = input.nextInt();
         switch (menu){
             case 1:
                 System.out.println("Redirecting you to short words...");
+
                 break;
             case 2:
                 System.out.println("Redirecting you to medium words...");
@@ -27,7 +31,21 @@ public class hangmangame {
                 break;
 
         }
+            public static String shortWord(String word) {
+            word = hangmangame.shortwords[(int) (Math.random() * shortwords.length)];
+            return word;}
 
+            public static String mediumWord(String word) {
+            word = hangmangame.mediumwords[(int) (Math.random() * mediumwords.length)];
+            return word;
+        }
+            public static String longWord(String word) {
+            word = hangmangame.mediumwords[(int) (Math.random() * mediumwords.length)];
+            return word;
+        }
+    public static String chooseWord(hangmangame.shortwords, mediumwords, longwords){
+
+        }
 
     }
 
