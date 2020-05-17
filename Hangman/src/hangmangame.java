0668c0c60730edbd2ea2490161133344f13ea603
oggep <54796870+oggep@users.noreply.github.com>
2020-05-17 14:01:31 +0200

    Fick ett till problem

--- Hangman/src/hangmangame.java
+++ Hangman/src/hangmangame.java
@@ -3,14 +3,16 @@ import java.util.Random;
 
 public class hangmangame {
     private static int menu = 0;
-    private static String[] shortwords = {"car", "foot", "rain", "sand", "cow", "shoe","dirt"};
+    private static String[] shortwords = {"car", "foot", "rain", "sand", "cow", "shoe", "dirt"};
     private static String[] mediumwords = {"forest", "bottle", "teeth", "bored", "forced", "knife"};
     private static String[] longwords = {"skydiving", "Communicate", "friendzone", "vandalize", "shockproof", "advanced"};
     private static String word = "";
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         startMenu();
     }
-    public static void startMenu(){
+
+    public static void startMenu() {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to Hangman");
         System.out.println("What length of words do you want?");
@@ -18,7 +20,7 @@ public class hangmangame {
         System.out.println("2. Medium");
         System.out.println("3. Long");
         menu = input.nextInt();
-        switch (menu){
+        switch (menu) {
             case 1:
                 System.out.println("Redirecting you to short words...");
                 shortWord();
@@ -33,28 +35,38 @@ public class hangmangame {
                 break;
         }
     }
+
     private static void shortWord() {
         Random rand = new Random();
         word = shortwords[rand.nextInt(shortwords.length)];
         chooseWord();
     }
+
     private static void mediumWord() {
         Random rand = new Random();
         word = mediumwords[rand.nextInt(mediumwords.length)];
         chooseWord();
     }
-        
+
     private static void longWord() {
         Random rand = new Random();
         word = longwords[rand.nextInt(longwords.length)];
-        chooseWord();
+        wordScan();
     }
-             public static void chooseWord(){
-                char[] doeslettermatch = word.toCharArray();
-                
-                for (char output : doeslettermatch) {
-                System.out.println(output);
+
+    public static void chooseWord() {
+        char[] chararray = word.toCharArray();
+
+        for (char output : chararray) {
+            System.out.println(output);
+        Scanner input = new Scanner(System.in);
+        char doeslettermatch = input.next().charAt(0);
+        for (doeslettermatch : word.length()) {
+            if(doeslettermatch == chararray){
+                System.out.println(doeslettermatch);
+            }
+
             }
         }
-
     }
+}
