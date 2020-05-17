9205bc23a3b0538f633a8e21456f10c0be36203e
oggep <54796870+oggep@users.noreply.github.com>
2020-05-17 20:47:10 +0200

    Lyckades lösa en massa problem

--- Hangman/src/hangmangame.java
+++ Hangman/src/hangmangame.java
@@ -8,7 +8,9 @@ public class hangmangame {
     private static String[] longwords = {"skydiving", "Communicate", "friendzone", "vandalize", "Football", "advanced"};
     private static String word;
     private static char doeslettermatch;
-    private static char[] rightletters;
+    private static char[] chararray;
+
+
 
 
 
@@ -45,47 +47,56 @@ public class hangmangame {
     private static void shortWord() {
         Random rand = new Random();
         word = shortwords[rand.nextInt(shortwords.length)];
+        chararray = new char[word.length()];
+        for (int i = 0; i < chararray.length; i++) {
+            chararray[i] = '_';}
         checkLetter();
     }
 
     private static void mediumWord() {
         Random rand = new Random();
         word = mediumwords[rand.nextInt(mediumwords.length)];
+        chararray = new char[word.length()];
+        for (int i = 0; i < chararray.length; i++) {
+            chararray[i] = '_';}
         checkLetter();
     }
 
     private static void longWord() {
         Random rand = new Random();
         word = longwords[rand.nextInt(longwords.length)];
-        checkLetter();
+        chararray = new char[word.length()];
+        for(int i = 0; i < chararray.length; i++) {
+            chararray[i] = '_';}
+        checkLetter(char[] chararray);
     }
 
-    public static void checkLetter() {
+    public static void checkLetter(char chararray1) {
         Scanner input = new Scanner(System.in);
         System.out.println("Choose a letter!");
+        System.out.println(chararray1);
 
 
-        char[] chararray = new char[word.length()];
+
         System.out.println(word);
 
-        doeslettermatch = input.next().toLowerCase().charAt(0);
-
-        for (int i = 0; i < chararray.length; i++) {
-            System.out.print("_");
-        }
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == doeslettermatch) {
-                rightletters[i] = doeslettermatch;
-        }
-
-                    //for (int i; i < rightletters; i++)
-                    //doeslettermatch == rightletters[i];
-
-                }
+        char letter = input.next().toLowerCase().charAt(0);
+            for(int j = 0; j < chararray.length; j++) {
+            if (word.charAt(j) == letter) {
+                chararray[j] = letter;
             }
 
+        }
 
 
 
-}
+
+               }
+        }
+    }
+
+
+
+
+
 
