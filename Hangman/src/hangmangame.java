910b89ea692ffca2e2bb05194a4c7f32a1dffa58
oggep <54796870+oggep@users.noreply.github.com>
2020-05-18 09:48:50 +0200

    Har fått ett nytt problem
    min setuphangman vill inte funka och vet ej varför.

--- Hangman/src/hangmangame.java
+++ Hangman/src/hangmangame.java
@@ -9,21 +9,25 @@ public class hangmangame {
     private static String[] longwords = {"skydiving", "Communicate", "friendzone", "vandalize", "Football", "advanced"};
     private static String word;
     private static char[] chararray;
-    private static ArrayList<String> hangmananimation = setuphangman();
+    private static ArrayList<String> hangmananimation = setUpHangman();
     private static Scanner input = new Scanner(System.in);
     private static Random rand = new Random();
 
     public static void main(String[] args) {
         word = "";
         startMenu();
-        while (true){
-            startMenu();
+        while (true) {
+            System.out.println("");
+            System.out.println("You lost!");
+            System.out.println("");
             System.out.println("Do you want to play again?");
-            int playagain =input.nextInt();
+            System.out.println("Press 1 if you want to play again.");
+            int playagain = input.nextInt();
             input.nextLine();
-            if(playagain != 1){
+            if (playagain != 1) {
                 break;
             }
+            startMenu();
         }
         System.out.println("Goodbye!");
     }
@@ -35,8 +39,13 @@ public class hangmangame {
         System.out.println("1. Short");
         System.out.println("2. Medium");
         System.out.println("3. Long");
+        System.out.println("Only numbers!");
         menu = input.nextInt();
         input.nextLine();
+        if (menu > 3) {
+            System.out.println("This difficulty doesn't exist. Automaticly switching you to short words.");
+            menu = 1;
+        }
         switch (menu) {
             case 1:
                 System.out.println("Redirecting you to short words...");
@@ -84,81 +93,88 @@ public class hangmangame {
         for (int tries = 1; tries <= 7; tries++) {
             System.out.println("Choose a letter!");
             char letter = input.nextLine().toLowerCase().charAt(0);
+            if (tries == 0) {
+                tries++;
+            }
             if (Character.isLetter(letter)) {
                 if (word.contains(letter + "")) {
                     for (int j = 0; j < chararray.length; j++) {
                         if (word.charAt(j) == letter) {
                             chararray[j] = letter;
                             tries--;
+                            for (int i = 0; i < chararray.length; i++) {
+                                if (chararray[i] == '_') ;
+                                {
+                                    boolean won = false;
+                                }
+
+                            }
                         }
                     }
+                } else {
+                    System.out.println(hangmananimation.get(tries - 1));
+                    System.out.println(7 - tries + " Wrong Guesses left");
                 }
-                else {
-                    System.out.println(hangmananimation.get(tries));
-                    System.out.println(7 - tries + " Guesses left")
-                }
+                System.out.println(chararray);
             }
 
-            System.out.println(chararray);
+
         }
 
 
-    }
+        private static ArrayList<String> setUpHangman(){
+            ArrayList<String> returnvalue = new ArrayList<>();
+            returnvalue.add("  +---+\n" +
+                    "  |   |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "=========''', '''");
+            returnvalue.add("  +---+\n" +
+                    "  |   |\n" +
+                    "  o   |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "=========''', '''");
+            returnvalue.add("  +---+\n" +
+                    "  |   |\n" +
+                    "  o   |\n" +
+                    "  |   |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "=========''', '''");
+            returnvalue.add("  +---+\n" +
+                    "  |   |\n" +
+                    "  o   |\n" +
+                    " /|   |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "=========''', '''");
+            returnvalue.add("  +---+\n" +
+                    "  |   |\n" +
+                    "  o   |\n" +
+                    " /|\\  |\n" +
+                    "      |\n" +
+                    "      |\n" +
+                    "=========''', '''");
+            returnvalue.add(" +---+\n" +
+                    "  |   |\n" +
+                    "  o   |\n" +
+                    " /|\\  |\n" +
+                    " /    |\n" +
+                    "      |\n" +
+                    "=========''', '''");
+            returnvalue.add("  +---+\n" +
+                    "  |   |\n" +
+                    "  o   |\n" +
+                    " /|\\  |\n" +
+                    " / \\  |\n" +
+                    "      |\n" +
+                    "=========''']");
+            return returnvalue;
+        }
 
-    private static ArrayList<String> setuphangman() {
-        ArrayList<String> returnvalue = new ArrayList<>();
-        returnvalue.add("  +---+\n" +
-                "  |   |\n" +
-                "      |\n" +
-                "      |\n" +
-                "      |\n" +
-                "      |\n" +
-                "=========''', '''");
-        returnvalue.add("  +---+\n" +
-                "  |   |\n" +
-                "  o   |\n" +
-                "      |\n" +
-                "      |\n" +
-                "      |\n" +
-                "=========''', '''");
-        returnvalue.add("  +---+\n" +
-                "  |   |\n" +
-                "  o   |\n" +
-                "  |   |\n" +
-                "      |\n" +
-                "      |\n" +
-                "=========''', '''");
-        returnvalue.add("  +---+\n" +
-                "  |   |\n" +
-                "  o   |\n" +
-                " /|   |\n" +
-                "      |\n" +
-                "      |\n" +
-                "=========''', '''");
-        returnvalue.add("  +---+\n" +
-                "  |   |\n" +
-                "  o   |\n" +
-                " /|\\  |\n" +
-                "      |\n" +
-                "      |\n" +
-                "=========''', '''");
-        returnvalue.add(" +---+\n" +
-                "  |   |\n" +
-                "  o   |\n" +
-                " /|\\  |\n" +
-                " /    |\n" +
-                "      |\n" +
-                "=========''', '''");
-        returnvalue.add("  +---+\n" +
-                "  |   |\n" +
-                "  o   |\n" +
-                " /|\\  |\n" +
-                " / \\  |\n" +
-                "      |\n" +
-                "=========''']");
-        return returnvalue;
-    }
-    private static void end(){
-        //här ska man gå till while spela igen men spelet krashar ska försöka lösa det imon.
     }
 }
